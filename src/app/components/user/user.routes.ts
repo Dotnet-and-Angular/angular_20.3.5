f01ad1238ed9466f167d2442e8310131d61b35b3
Gunasekhar Raju <gunasekhar.chikili54@gmail.com>
2025-12-01 12:19:27 +0530

    format code

--- src/app/components/user/user.routes.ts
+++ src/app/components/user/user.routes.ts
@@ -1,36 +1,47 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 import {
-    AnalyticsComponent, UserDashboard, EditProfileComponent, ProfileComponent, ViewProfileComponent,
-    AccountSettingsComponent, Userdata, SettingsComponent, PrivacySettingsComponent, NotificationSettingsComponent
-} from "@user/components";
+    AnalyticsComponent,
+    UserDashboard,
+    EditProfileComponent,
+    ProfileComponent,
+    ViewProfileComponent,
+    AccountSettingsComponent,
+    Userdata,
+    SettingsComponent,
+    PrivacySettingsComponent,
+    NotificationSettingsComponent,
+} from '@user/components';
 
 export const USER_ROUTES: Routes = [
     {
-        path: '', component: UserDashboard,
+        path: '',
+        component: UserDashboard,
         children: [
             { path: 'user-data', component: Userdata },
             {
-                path: 'settings', component: SettingsComponent,
+                path: 'settings',
+                component: SettingsComponent,
                 children: [
                     { path: 'account', component: AccountSettingsComponent },
                     { path: 'privacy', component: PrivacySettingsComponent },
                     { path: 'notifications', component: NotificationSettingsComponent },
-                    { path: '', redirectTo: 'account', pathMatch: 'full' }
-                ]
+                    { path: '', redirectTo: 'account', pathMatch: 'full' },
+                ],
             },
             {
-                path: 'profile', component: ProfileComponent,
+                path: 'profile',
+                component: ProfileComponent,
                 children: [
                     { path: 'view', component: ViewProfileComponent },
                     { path: 'edit', component: EditProfileComponent },
-                    { path: '', redirectTo: 'view', pathMatch: 'full' }
-                ]
+                    { path: '', redirectTo: 'view', pathMatch: 'full' },
+                ],
             },
             {
                 path: 'analytics',
-                component: AnalyticsComponent
+                component: AnalyticsComponent,
             },
-            { path: '', redirectTo: 'user-data', pathMatch: 'full' }
-        ]
+            { path: '', redirectTo: 'user-data', pathMatch: 'full' },
+        ],
     },
 ];
